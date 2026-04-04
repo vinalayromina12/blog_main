cf0b0e19fd8170f2d49ba7b503f3e0021bbbfaf5
vinalayromina12 <vinalayromina12@gmail.com>
2026-04-04 00:39:34 -0700

    Terminar

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,38 @@
-from django.contrib import admin
+#--*--ciding:-utf-8--*-
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Category, Comment, Post
+
+
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['post']
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ['category', 'created_at', 'status']
+    inlines = [CommentItemInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created at', 'status')
+    list_filter = ['category', 'created at', 'status']
+    inlines = [CommentItemInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ('title', )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, detail
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('blog/<int:id', detail, name='post_detail'),
+]
